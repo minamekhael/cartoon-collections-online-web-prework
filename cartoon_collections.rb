f29398fb0dfbd7065bac1b61261ced74047442de
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-08 20:44:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ end
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls = ["apple", "banana", "orange"]
-  planeteer_calls.each {|n| print n.capitalize + "!"}
+  planeteer_calls.each {|n| print n.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
