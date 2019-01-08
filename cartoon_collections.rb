395b1995aa7da60eee34a2ccd51c076f147391d2
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-08 20:45:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.each {|n| print n.capitalize}
+  planeteer_calls.each {|n| puts n.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
