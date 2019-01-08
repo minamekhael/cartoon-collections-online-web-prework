dc117e91a11a917e5f454f4e233bbd4e1c7e7569
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-08 21:19:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |type|
+    cheese.find do |type|
     cheese_types.include?(type)
   end 
 end
