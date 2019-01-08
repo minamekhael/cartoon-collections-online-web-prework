05f1208010d773ab53eca093923633cafd1e1399
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-08 20:49:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,3 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.each {|n| puts n.capitalize + "!"}
 end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
