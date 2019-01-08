9681a6b645acc63a5a726714086629bd7441c196
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-08 21:18:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,9 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese(cheese)
-  cheese_types = ["cheddar", "gouda", "cambert"]
-  array.find do |type|
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
     cheese_types.include?(type)
   end 
 end
